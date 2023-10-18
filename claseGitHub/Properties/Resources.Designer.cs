89fa34d14034c69556239539dd5b0bec265bd392
juanpanpanyz <juanbaader@gmail.com>
2023-10-18 10:02:33 -0300

    Dario
    Foto de dario muy fachera

--- claseGitHub/Properties/Resources.Designer.cs
+++ claseGitHub/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace claseGitHub.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap unnamed {
+            get {
+                object obj = ResourceManager.GetObject("unnamed", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
